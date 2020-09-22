5fba80b28ecae2a117915e1fa860ceb0bada8fa0
DanConstantin06 <32301499+DanConstantin06@users.noreply.github.com>
2020-09-22 10:20:43 +0300

    Add files via upload

---
+++ ToyRobotSimulator/AppShell.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using ToyRobotSimulator.ViewModels;
+using ToyRobotSimulator.Views;
+using Xamarin.Forms;
+
+namespace ToyRobotSimulator
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+        }
+
+    }
+}
